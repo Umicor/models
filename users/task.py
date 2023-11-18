5ff7614f9c5f2ac0287b2353f6ca07dae6177507
Lenovo <fogotstop@gmail.com>
2023-11-19 00:35:01 +0300

    celery

--- users/task.py
+++ users/task.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
-#from  import celery_app
+from supsite.celery import celery_app
 
-#@celery_app.task()
+@celery_app.task()
 def send_email_task(subject, message, from_email, recipient_list):
     send_mail(subject, message, from_email, recipient_list)
+#pip install pip-tools
