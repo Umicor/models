928ee900d87b24f436412553da8a11ea4b0db395
Lenovo <fogotstop@gmail.com>
2023-11-23 19:33:12 +0300

    pipenv

--- supsite/settings.py
+++ supsite/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY")
+#SECRET_KEY = config("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#!!!!!!!
 DEBUG = config("DJANG0_DEBUG")
+#DEBUG = strtobool(os.getenv("DJANG0_DEBUG"))
 
 ALLOWED_HOSTS = [config("DJANGO_ALLOWED_HOSTS")]
 
